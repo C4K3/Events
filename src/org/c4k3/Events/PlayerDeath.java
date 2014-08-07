f079d29309ad9c46be0d6000393bf3ee40da45eb
C4K3 <sebastian@c4k3.org>
2014-08-07 21:06:05 +0200

    Fix players being able to /kill after restart to leave with gm1

--- src/org/c4k3/Events/PlayerDeath.java
+++ src/org/c4k3/Events/PlayerDeath.java
@@ -1,5 +1,7 @@
 package org.c4k3.Events;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -7,6 +9,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 /** Class is responsible for listening to player deaths
  * and making sure no xp is dropped in the events
+ * and that players don't keep any set gamemodes
  */
 public class PlayerDeath implements Listener {
 
@@ -16,6 +19,14 @@ public class PlayerDeath implements Listener {
 		if ( Event.isPlayerActive(event.getEntity().getUniqueId()) ) {
 
 			event.setDroppedExp(0);
+
+		}
+
+		Player player = event.getEntity();
+
+		if ( !player.isOp() && player.getGameMode() != GameMode.SURVIVAL ) {
+
+			player.setGameMode(GameMode.SURVIVAL);
 
 		}
 
